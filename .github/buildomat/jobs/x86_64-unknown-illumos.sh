7c29100c92e903b1ec2a912ee6f7906406d27c98
Rain <rain@sunshowers.io>
2024-11-06 21:51:25 +0000

    test illumos CI

--- .github/buildomat/jobs/x86_64-unknown-illumos.sh
+++ .github/buildomat/jobs/x86_64-unknown-illumos.sh
@@ -3,6 +3,7 @@
 #: name = "x86_64-unknown-illumos"
 #: variety = "basic"
 #: target = "helios-latest"
-#: rust_toolchain = "stable"
 
+# Note: we use ci/install-rust.sh rather than buildomat to install Rust, for
+# consistency with other CI jobs.
 exec .github/buildomat/build-and-test.sh illumos
